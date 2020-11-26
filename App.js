0dc7196d3b25a9564c9afe4b1cc55b2fd9ccdb5f
nilslambertz <nilslambertz@web.de>
2020-11-26 23:04:53 +0100

    added Subject-Buttons

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Button, StatusBar, Platform } from 'react-native';
+import SubjectButton from './Components/SubjectButton';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>:)</Text>
+      <SubjectButton reached="20" needed="40" title="Mathematik" count="3"></SubjectButton>
+      <SubjectButton reached="60" needed="30" title="Diskrete Strukturen" count="1"></SubjectButton>
       <ExpoStatusBar style="auto" />
     </SafeAreaView>
   );
@@ -16,5 +18,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-   },
+   }
 });

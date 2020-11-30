e51c8a783028da05e47a63ca749056707c122240
nilslambertz <nilslambertz@web.de>
2020-12-01 00:26:33 +0100

    added functions to create new subject, count gets incremented if successful, added function to delete all data

--- src/Utils/FileManagement.js
+++ src/Utils/FileManagement.js
@@ -18,11 +18,16 @@ function writeFile(filename, jsonContent) {
     return FileSystem.writeAsStringAsync(filename, JSON.stringify(jsonContent), { encoding: FileSystem.EncodingType.UTF8});
 }
 
+function resetAll() {
+    FileSystem.deleteAsync(configFile);
+    FileSystem.deleteAsync(subjectFile);
+}
+
 export function initialize() {
     if(config === undefined || subjects === undefined) {
         console.log("Loading files...");
         load(configFile, "config", initialConfig).then((c) => {config = c;}).catch(err => console.log(err));
-        load(subjectFile, "subjects", initialSubjects).then((c) => {subjects = c;}).catch(err => console.log(err));
+        load(subjectFile, "subjects", initialSubjects).then((c) => {subjects = c; console.log(subjects)}).catch(err => console.log(err));
     } else {
         console.log("Already loaded.");
     }
@@ -38,14 +43,15 @@ function load(file, title, defaultContent) {
                     console.log("Loaded " + title + " successfully");
                 }).catch(() => {
                     reject("Error while reading " + title + ". Path: " + file);
-                })
+                });
             } else {
                 let p = writeFile(file, defaultContent);
                 p.then((c) => {
                     resolve(defaultContent);
+                    console.log("Created " + title);
                 }).catch(() => {
                     reject("Error while writing " + title + ". Path: " + file);
-                })
+                });
             }
         })
     });
@@ -59,4 +65,35 @@ export function titleExists(t) {
         }
     }
     return false;
+}
+
+export function addSubject(newTitle, newProzent, newAnzahl) {
+    newTitle = newTitle.trim();
+    if(titleExists(newTitle)) return false;
+    newProzent = parseInt(newProzent);
+    newAnzahl = parseInt(newAnzahl);
+
+    let newId = config.idCount;
+    
+    return new Promise((resolve, reject) => {
+        subjects[newId] = {
+            title: newTitle,
+            needed: newProzent,
+            number: newAnzahl 
+        };
+        let p = writeFile(subjectFile, subjects);
+        p.then((c) => {
+            resolve(subjects);
+            console.log("Created subject with title " + newTitle);
+            config.idCount = newId + 1;
+            let p = writeFile(configFile, config);
+            p.then((c) => {
+                console.log("Incremented subject count to " + config.idCount);
+            }).catch(() => {
+                reject("Error while writing to to " + configFile + "\nError: " + err);
+            })
+        }).catch((err) => {
+            reject("Error while writing to " + subjectFile + "\nError: " + err);
+        });
+    });
 }

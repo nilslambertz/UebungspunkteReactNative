616bc1130c7f6ccb9121979cc1b1c480d6f6e8fd
nilslambertz <nilslambertz@web.de>
2020-12-02 00:42:50 +0100

    added screen for displaying information for one subject and exercises

---
+++ src/Components/SubjectScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ScrollView, Button, StatusBar, Platform, TouchableHighlight, Modal, Alert, TextInput } from 'react-native';
+
+class SubjectScreen extends Component {  
+    render() {
+        return (
+            <View>
+                <Text>I bims 1 subject</Text>
+            </View>
+        )
+    };
+}
+
+export default SubjectScreen;

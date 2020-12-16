4cb0d463aaf2dd8dd30197b25b5ea3244339c184
nilslambertz <nilslambertz@web.de>
2020-12-17 00:12:21 +0100

    changed way to list all exercises, now done with indidual keys every time it updates to force the refresh. added functions to delete and add exercises

--- src/Components/SubjectScreen.js
+++ src/Components/SubjectScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Modal, Alert, TextInput } from 'react-native';
+import { changeExercisePoints, deleteExercise, addExercise } from '../Utils/FileManagement';
 import ExerciseView from './ExerciseView';
 
 class SubjectScreen extends Component {  
@@ -8,23 +9,107 @@ class SubjectScreen extends Component {
       let params = props.route.params;
       this.state = {
           id: params.id,
-          subject: params.subject
-      }
+          subject: params.subject,
+          updateKey: 0,
+          newExercise: false
+        }
     }
+
+    
 
     componentDidMount() {
         this.props.navigation.setOptions({title: this.state.subject.title});
     }
 
+    changePoints = (index, points, max) => {
+        let p = changeExercisePoints(this.state.id, index, points, max);
+        p.then((c) => {
+            this.setState({subject: c}, () => {
+                let lolxd = parseInt(this.state.updateKey) + 1;
+                this.setState({updateKey: lolxd});
+            });
+        }).catch((err) => {
+            console.log(err);
+            alert("Fehler beim Ändern der Punkte!");
+        })
+    };
+
+    requestExerciseDelete = (index) => {
+        let p = deleteExercise(this.state.id, index);
+
+        p.then((c) => {
+            this.setState({subject: c}, () => {
+                let lolxd = parseInt(this.state.updateKey) + 1;
+                this.setState({updateKey: lolxd});
+            });
+        }).catch((err) => {
+          alert("Fehler beim Löschen des Fachs!");
+          console.log(err);
+        })
+      }
+
+    exerciseDeleteAlert = (index) => {
+        Alert.alert(
+            'Warnung',
+            'Willst du diese Übung wirklich löschen?',
+            [
+                {
+                    text: 'Ja, löschen',
+                    style: 'destructive',
+                    onPress: () => this.requestExerciseDelete(index)
+                },
+                {
+                    text: 'Nein, abbrechen',
+                    style: 'cancel'
+                }
+            ]
+        )
+    }
+
+    newExercisePress = () => {
+        this.setState({newExercise: true});
+    }
+
+    requestExerciseAdd = (index, points, max) => {
+        let p = addExercise(this.state.id, index, points, max);
+        p.then((c) => {
+            this.setState({subject: c}, () => {
+                let lolxd = parseInt(this.state.updateKey) + 1;
+                this.setState({updateKey: lolxd}, () => {
+                    this.setState({newExercise: false});
+                });
+            });
+        }).catch((err) => {
+            console.log(err);
+            alert("Fehler beim Ändern der Punkte!");
+        })
+    }
+
     render() {
+        const exerciseList = [];
+        let len = this.state.subject.exercises.length;
+
+        for(let i = 0; i < len; i++) {
+            let c = this.state.subject.exercises[i];
+            exerciseList.push(<ExerciseView key={this.state.updateKey.toString() + "-" + i.toString()} index={i} points={c[0]} max={c[1]} pressFunction={this.changePoints} longPressFunction={this.exerciseDeleteAlert}></ExerciseView>)
+        }
+
+        let maxPt = (len == 0 ? 10 : this.state.subject.exercises[len-1][1]);
+        
+
         return (
             <View style={styles.container}>
                <ScrollView contentContainerStyle={styles.scrollViewStyle}>
-                   <ExerciseView></ExerciseView>
+                  {
+                     exerciseList
+                  }
+                  { this.state.newExercise == true &&
+                      <ExerciseView key={"newExercise"} index={len} points={0} max={maxPt} pressFunction={this.requestExerciseAdd} longPressFunction={() => {}}></ExerciseView>
+                  }
                 </ScrollView>
                 <TouchableOpacity style={styles.newExerciseButton} onPress={this.newSubjectPress}>
                     <View>
-                        <Text style={styles.newExerciseButtonText}>Übung hinzufügen</Text>
+                        <Text style={styles.newExerciseButtonText} onPress={this.newExercisePress}>Übung hinzufügen</Text>
                     </View>
                 </TouchableOpacity>
             </View>

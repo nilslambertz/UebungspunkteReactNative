4517d88341b38aa10f21bc4a99cfc2bfea4b3810
nilslambertz <nilslambertz@web.de>
2020-12-18 02:53:12 +0100

    created const for headerStyle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Main from './Components/Main';
@@ -13,19 +14,19 @@ function App() {
     <NavigationContainer>
       <ExpoStatusBar style="auto" />
       <Stack.Navigator>
-        <Stack.Screen name="Übersicht" component={Main} options={{headerStyle: {
-            backgroundColor: '#EDEDED',
-            borderColor: "black",
-            borderBottomWidth: 1
-          }}} />
-          <Stack.Screen name="SubjectScreen" component={SubjectScreen} options={{headerStyle: {
-            backgroundColor: '#EDEDED',
-            borderColor: "black",
-            borderBottomWidth: 1
-          }}} />
+        <Stack.Screen name="Übersicht" component={Main} options={{headerStyle: styles.headerStyle}} />
+        <Stack.Screen name="SubjectScreen" component={SubjectScreen} options={{headerStyle: styles.headerStyle}} />
       </Stack.Navigator>
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  headerStyle: {
+    backgroundColor: '#EDEDED',
+    borderColor: "black",
+    borderBottomWidth: 1
+  }
+});
 
 export default App;

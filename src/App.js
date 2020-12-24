c2af1b800a3bee7f30274c1849cbcea155bbb053
nilslambertz <nilslambertz@web.de>
2020-12-24 01:13:34 +0100

    added Settings-screen to StackNavigator and button on headerRight in Overview-screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import 'react-native-gesture-handler';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import {Button, StyleSheet, View} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Main from './Components/Main';
 import SubjectScreen from './Components/SubjectScreen';
+import Settings from "./Components/Settings";
 
 const Stack = createStackNavigator();
 
@@ -14,8 +15,12 @@ function App() {
     <NavigationContainer>
       <ExpoStatusBar style="auto" />
       <Stack.Navigator>
-        <Stack.Screen name="Übersicht" component={Main} options={{headerStyle: styles.headerStyle}} />
+        <Stack.Screen name="Übersicht" component={Main} options={({navigation, route}) => ({headerStyle: styles.headerStyle, headerRight: (p) => (
+            <View style={{marginRight: 10}}><Button title="Einstellungen" onPress={() => {
+                navigation.navigate("Einstellungen");
+        }}/></View>)})} />
         <Stack.Screen name="SubjectScreen" component={SubjectScreen} options={{headerStyle: styles.headerStyle}} />
+        <Stack.Screen name="Einstellungen" component={Settings} options={{headerStyle: styles.headerStyle}} />
       </Stack.Navigator>
     </NavigationContainer>
   )

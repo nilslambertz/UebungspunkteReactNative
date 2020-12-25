b21de8654f22a4b984073091235c7216ac7de132
nilslambertz <nilslambertz@web.de>
2020-12-25 23:40:19 +0100

    added SettingsItem-Component

---
+++ src/Components/SettingsItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ScrollView, Alert} from 'react-native';
+
+class SettingsItem extends Component {
+    render() {
+        return (
+            <View><Text>moin</Text></View>
+        )
+    }
+}
+
+export default SettingsItem;

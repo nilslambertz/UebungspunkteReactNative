b3beffcbbfa1b5549c5e7fa3a5d793403eee62e7
nilslambertz <nilslambertz@web.de>
2020-12-27 21:18:28 +0100

    added basic style and items to SettingsItem

--- src/Components/SettingsItem.js
+++ src/Components/SettingsItem.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ScrollView, Alert} from 'react-native';
+import { StyleSheet, Text, View, Alert, Switch} from 'react-native';
 
-class SettingsItem extends Component {
-    render() {
-        return (
-            <View><Text>moin</Text></View>
-        )
-    }
+export default function SettingsItem({title, description, value}) {
+    return (
+        <View style={styles.settingsItem}>
+            <View style={styles.settingsItemLeft}>
+                <Text style={styles.settingsItemTitle}>Light-Theme</Text>
+                <Text style={styles.settingsItemDescription}>Helles Design aktiviert</Text>
+            </View>
+            <View style={styles.settingsItemRight}>
+                <Switch />
+            </View>
+        </View>
+    )
 }
 
-export default SettingsItem;
+const styles = StyleSheet.create({
+    settingsItem: {
+        borderBottomWidth: 1,
+        marginHorizontal: 5,
+        borderColor: "black",
+        flexDirection: "row",
+    },
+    settingsItemLeft: {
+        borderColor: "black",
+        padding: 5,
+        flex: 1
+    },
+    settingsItemTitle: {
+        fontSize: 30
+    },
+    settingsItemDescription: {
+        fontSize: 15,
+        color: "#606060"
+    },
+    settingsItemRight: {
+        justifyContent: 'center',
+        paddingLeft: 10,
+        marginVertical: 10,
+        borderColor: "#D2D2D2",
+        borderLeftWidth: 1
+    }
+});

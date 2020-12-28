5443f85849926dd17d6fdc311e7127c39d165e24
nilslambertz <nilslambertz@web.de>
2020-12-28 21:33:36 +0100

    list of settings-items is now printed

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, ScrollView, Alert} from 'react-native';
 import SettingsItem from "./SettingsItem";
+import {getSettings} from "../Utils/FileManagement";
 
 class Settings extends Component {
+    constructor(props) {
+        super(props);
+        let p = getSettings();
+        this.state = {
+            settings: {}
+        }
+        p.then((c) => {
+            this.setState({settings: c});
+        })
+    }
+
+    printSettingsList = () => {
+        let settings = this.state.settings;
+        return Object.keys(settings).map(function(c, i) {
+            let obj = settings[c];
+            console.log(obj);
+            return <SettingsItem key={i} value={obj["value"]} description={obj["description"]} title={obj["title"]} />
+        });
+    }
+
     render() {
         return (
-            <SettingsItem />
+            <View>
+                { this.printSettingsList() }
+            </View>
         )
     }
 }

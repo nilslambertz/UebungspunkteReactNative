424ce83c3c5c8f84c6d6317a96acea0ff3911d4d
nilslambertz <nilslambertz@web.de>
2020-12-29 00:20:48 +0100

    added style-folder and file where all style-information will be stored in the future

---
+++ src/Style/style.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    scrollViewStyle: {
+        padding: 5,
+        paddingBottom: 65
+    },
+    newSubjectButton: {
+        bottom: 10,
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderRadius: 2,
+        alignSelf: "center",
+        borderWidth: 1,
+        position: "absolute",
+    },
+    newSubjectButtonText: {
+        fontSize: 20,
+    }
+});

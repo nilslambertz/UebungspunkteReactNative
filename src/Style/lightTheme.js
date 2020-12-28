6d0ebc32e71a5f2853ecd18f422847ad30ab771e
nilslambertz <nilslambertz@web.de>
2020-12-29 00:48:32 +0100

    added file for light-theme-style

---
+++ src/Style/lightTheme.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        backgroundColor: 'white'
+    },
+    newSubjectButton: {
+        backgroundColor: "#32CD32",
+        borderColor: "gray",
+    },
+    newSubjectButtonText: {
+        color: "black"
+    }
+});

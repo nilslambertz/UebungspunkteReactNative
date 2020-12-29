acc5f184077a5ceb53f95a1809c56875bf9e610f
nilslambertz <nilslambertz@web.de>
2020-12-29 01:43:15 +0100

    added darkTheme-file

---
+++ src/Style/darkTheme.js
@@ -0,0 +1,95 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        backgroundColor: 'white'
+    },
+    containerScrollView: {
+
+    },
+    bottomButton: {
+        backgroundColor: "#32CD32",
+        borderColor: "gray",
+    },
+    bottomButtonText: {
+        color: "black"
+    },
+    subjectButtonContainer: {
+        backgroundColor: 'white',
+        borderColor: '#5C5C5C',
+    },
+    subjectButtonPercentView: {
+        borderColor: "#5C5C5C"
+    },
+    subjectButtonBigText: {
+        color: "black"
+    },
+    subjectButtonSmallText: {
+        color: "#7D7D7D"
+    },
+    subjectButtonTitle: {
+    },
+    popupShadow: {
+        backgroundColor: "rgba(0,0,0,0.7)"
+    },
+    popupContainer: {
+        backgroundColor: "#EFEFEF",
+    },
+    popupButtonView: {
+    },
+    popupHeader: {
+        color: "black",
+    },
+    popupSection: {
+    },
+    popupInput: {
+        borderColor: 'gray',
+        color: "black",
+    },
+    popupText: {
+        color: "black",
+    },
+    popupError: {
+        color: "red",
+    },
+    settingsResetView: {
+    },
+    settingsResetButton: {
+    },
+    settingsItem: {
+        borderColor: "black",
+    },
+    settingsItemLeft: {
+    },
+    settingsItemTitle: {
+        color: "black"
+    },
+    settingsItemDescription: {
+        color: "#606060"
+    },
+    settingsItemRight: {
+        borderColor: "#D2D2D2",
+    },
+    exerciseViewContainer: {
+        borderColor: '#5C5C5C',
+        backgroundColor: 'white'
+    },
+    newExercise: {
+        backgroundColor: "#EBB1B1"
+    },
+    exerciseNumber: {
+        borderColor: "#5C5C5C"
+    },
+    exerciseNumberText: {
+    },
+    exerciseContent: {
+    },
+    exerciseDivider: {
+    },
+    exerciseInput: {
+        borderColor: "gray",
+    },
+    exerciseOkayButton: {
+        borderColor: "#5C5C5C"
+    }
+});

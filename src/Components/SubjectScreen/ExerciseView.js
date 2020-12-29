4e66b6a68d198b0522b47b383f1763866848d86d
nilslambertz <nilslambertz@web.de>
2020-12-29 01:36:41 +0100

    implemented theme in ExerciseView

--- src/Components/SubjectScreen/ExerciseView.js
+++ src/Components/SubjectScreen/ExerciseView.js
@@ -2,32 +2,32 @@ import React, { useState } from 'react';
 import { Text, TouchableOpacity, View, Button, TextInput } from 'react-native';
 import style from '../../Style/style';
 
-export default function ExerciseView({index, points, max, pressFunction, longPressFunction, newExercise}) {
+export default function ExerciseView({index, points, max, pressFunction, longPressFunction, newExercise, theme}) {
     const [pointText, setPointText] = useState(points);
     const [maxText, setMaxText] = useState(max);
     const [confirmed, setConfirmed] = useState(true);
 
-    const containerStyle = (newExercise ? [style.exerciseViewContainer, style.newExercise] : style.exerciseViewContainer);
+    const containerStyle = (newExercise ? [style.exerciseViewContainer, theme.exerciseViewContainer, theme.newExercise] : [style.exerciseViewContainer, theme.exerciseViewContainer]);
 
     return (
         <TouchableOpacity onLongPress={() => {longPressFunction(index)}}>
             <View style={containerStyle}>
-                <View style={style.exerciseNumber}>
-                    <Text style={style.exerciseNumberText}>{index+1}</Text>
+                <View style={[style.exerciseNumber, theme.exerciseNumber]}>
+                    <Text style={[style.exerciseNumberText, theme.exerciseNumberText]}>{index+1}</Text>
                 </View>
-                <View style={style.exerciseContent}>
-                    <TextInput style={style.exerciseInput} keyboardType='numeric' onChangeText={t => {
+                <View style={[style.exerciseContent, theme.exerciseContent]}>
+                    <TextInput style={[style.exerciseInput, theme.exerciseInput]} keyboardType='numeric' onChangeText={t => {
                         setPointText(t);
                         setConfirmed(false);
                     }}>{pointText}</TextInput>
-                    <Text style={style.exerciseDivider}>/</Text>
-                    <TextInput style={style.exerciseInput} keyboardType='numeric' onChangeText={t => {
+                    <Text style={[style.exerciseDivider, theme.exerciseDivider]}>/</Text>
+                    <TextInput style={[style.exerciseInput, theme.exerciseInput]} keyboardType='numeric' onChangeText={t => {
                         setMaxText(t);
                         setConfirmed(false);
                     }}>{maxText}</TextInput>
                 </View>
                 { !confirmed &&
-                <View style={style.exerciseOkayButton}>
+                <View style={[style.exerciseOkayButton, theme.exerciseOkayButton]}>
                     <Button title={"Speichern"} onPress={() => {
                         setConfirmed(true);
                         pressFunction(index, parseFloat(pointText + ""), parseFloat(maxText + ""));

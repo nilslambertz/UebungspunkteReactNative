411d201a3f9ef63b202891c4b7548f85ab448fd6
nilslambertz <nilslambertz@web.de>
2020-12-30 21:28:26 +0100

    changed transition between screens to default-ios-transition

--- src/App.js
+++ src/App.js
@@ -3,24 +3,12 @@ import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import {Button, StyleSheet, View} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
 import Main from './Components/Main/Main';
 import SubjectScreen from './Components/SubjectScreen/SubjectScreen';
 import Settings from "./Components/Settings/Settings";
 
 const Stack = createStackNavigator();
-
-const config = {
-    animation: 'spring',
-    config: {
-        stiffness: 1000,
-        damping: 500,
-        mass: 5,
-        overshootClamping: true,
-        restDisplacementThreshold: 0.01,
-        restSpeedThreshold: 0.01,
-    },
-};
 
 function App() {
   return(
@@ -30,21 +18,12 @@ function App() {
         <Stack.Screen name="Übersicht" component={Main} options={({navigation, route}) => ({headerRight: (p) => (
             <View style={{marginRight: 10}}><Button title="Einstellungen" onPress={() => {
                 navigation.navigate("Einstellungen");
-        }}/></View>), transitionSpec: {
-            open: config,
-            close: config,
-        }})} />
+        }}/></View>), cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS })} />
         <Stack.Screen name="SubjectScreen" component={SubjectScreen} options={{
-            transitionSpec: {
-                open: config,
-                close: config,
-            },
+            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS
         }}/>
         <Stack.Screen name="Einstellungen" component={Settings} options={{
-            transitionSpec: {
-                open: config,
-                close: config,
-            },
+            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS
         }}/>
       </Stack.Navigator>
     </NavigationContainer>
